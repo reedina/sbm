210a3cbf65929c9068f9698c5cf537138aaeaaac
Reedina <reedinafarms@gmail.com>
2017-09-07 14:37:21 -0700

    added ctr.go db.go and environment files

---
+++ ctrl/environment.go
@@ -0,0 +1,117 @@
+package ctrl
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/reedina/buildenvironment/model"
+)
+
+//CreateEnvironment (POST)
+func CreateEnvironment(w http.ResponseWriter, r *http.Request) {
+	var environment model.Environment
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&environment); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	//Does the Environment Attribute Resource Exist ?
+	if model.DoesEnvironmentResourceExist(&environment) == true {
+		respondWithError(w, http.StatusConflict, "Resource already exists")
+		return
+	}
+
+	//Resource does not exist, go ahead and create resource
+	if err := model.CreateEnvironment(&environment); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, environment)
+}
+
+//GetEnvironments  (GET)
+func GetEnvironments(w http.ResponseWriter, r *http.Request) {
+
+	environments, err := model.GetEnvironments()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, environments)
+}
+
+//GetEnvironment (GET)
+func GetEnvironment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Environment ID")
+		return
+	}
+
+	environment := model.Environment{ID: id}
+	if err := model.GetEnvironment(&environment); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Environment not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, environment)
+}
+
+//UpdateEnvironment (PUT)
+func UpdateEnvironment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Environment ID")
+		return
+	}
+
+	var environment model.Environment
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&environment); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	environment.ID = id
+
+	if err := model.UpdateEnvironment(&environment); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, environment)
+}
+
+//DeleteEnvironment (DELETE)
+func DeleteEnvironment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Environment ID")
+		return
+	}
+	environment := model.Environment{ID: id}
+
+	if err := model.DeleteEnvironment(&environment); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
---
+++ model/environment.go
@@ -0,0 +1,88 @@
+package model
+
+import "database/sql"
+
+//Environment  (TYPE)
+type Environment struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
+
+//Environments (TYPE)
+type Environments struct {
+	Environments []*Environment `json:"environments"`
+}
+
+//DoesEnvironmentResourceExist (POST)
+func DoesEnvironmentResourceExist(environment *Environment) bool {
+
+	err := db.QueryRow("SELECT id, name, description, type FROM environments WHERE name=$1 and type=$2",
+		environment.Name, environment.Type).Scan(&environment.ID, &environment.Name, &environment.Description, &environment.Type)
+
+	if err == sql.ErrNoRows {
+		return false
+	}
+
+	return true
+}
+
+//CreateEnvironment (POST)
+func CreateEnvironment(environment *Environment) error {
+
+	err := db.QueryRow(
+		"INSERT INTO environments(name, description, type) VALUES($1, $2, $3) RETURNING id",
+		environment.Name, environment.Description, environment.Type).Scan(&environment.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//GetEnvironments (GET)
+func GetEnvironments() ([]Environment, error) {
+	rows, err := db.Query("SELECT id, name, description, type FROM environments")
+
+	if err != nil {
+		return nil, err
+	}
+
+	environments := []Environment{}
+
+	for rows.Next() {
+		defer rows.Close()
+
+		var e Environment
+		if err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.Type); err != nil {
+			return nil, err
+		}
+		environments = append(environments, e)
+	}
+
+	return environments, nil
+}
+
+//GetEnvironment (GET)
+func GetEnvironment(environment *Environment) error {
+	return db.QueryRow("SELECT name, description, type FROM environments WHERE id=$1",
+		environment.ID).Scan(&environment.Name, &environment.Description, &environment.Type)
+}
+
+//UpdateEnvironment (PUT)
+func UpdateEnvironment(environment *Environment) error {
+	_, err :=
+		db.Exec("UPDATE environments SET name=$1, description=$2, type=$3 WHERE id=$4",
+			environment.Name, environment.Description, environment.Type, environment.ID)
+
+	return err
+}
+
+//DeleteEnvironment (DELETE)
+func DeleteEnvironment(environment *Environment) error {
+	_, err := db.Exec("DELETE FROM environments WHERE id=$1", environment.ID)
+
+	return err
+}
